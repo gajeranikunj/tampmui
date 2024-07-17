0b9682112bb413216ca5a9a0600f1033fc1a6329
sudipdholariya <sudipdholariya8@gmail.com>
2024-07-17 13:31:53 +0530

    add news

--- src/componats/News/News.js
+++ src/componats/News/News.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CNavbare from '../CNavbare'
 import Footer from '../Footer'
 import { Box, Container, Grid } from '@mui/material'
+import { MdKeyboardArrowRight } from "react-icons/md"
 
 function News() {
     return (
@@ -13,10 +14,26 @@ function News() {
                         <Grid item xs={12} md={9}>
                             1
                         </Grid>
-                        <Grid item xs={12} md={3}>
-                            <Box sx={{ backgroundColor: "white",padding:"20px" }}>
+                        <Grid item xs={12} md={3} sx={{ position: "sticky", top: "10px" }}>
+                            <Box sx={{ backgroundColor: "white", padding: "20px" }}>
                                 <Box >
-                                    <Box sx={{fontSize:"22px"}}>Categories</Box>
+                                    <Box sx={{ fontSize: "18px" }}>Categories</Box>
+                                </Box>
+                                <Box sx={{ color: "#6EC1E4", marginTop: "10px" }}>
+                                    <Box sx={{ marginTop: "5px" }}><MdKeyboardArrowRight />All</Box>
+                                    <Box sx={{ marginTop: "5px" }}><MdKeyboardArrowRight />Business</Box>
+                                    <Box sx={{ marginTop: "5px" }}><MdKeyboardArrowRight />Construction</Box>
+                                    <Box sx={{ marginTop: "5px" }}><MdKeyboardArrowRight />Real Estate</Box>
+                                </Box>
+                            </Box>
+
+                            <Box sx={{ backgroundColor: "white", padding: "20px", marginTop: "30px" }}>
+                                <Box >
+                                    <Box sx={{ fontSize: "18px" }}>Archives</Box>
+                                </Box>
+                                <Box sx={{ color: "#6EC1E4", marginTop: "10px" }}>
+                                    <Box sx={{ marginTop: "5px" }}><MdKeyboardArrowRight />March 2016</Box>
+                                    <Box sx={{ marginTop: "5px" }}><MdKeyboardArrowRight />January 2016</Box>
                                 </Box>
                             </Box>
                         </Grid>
